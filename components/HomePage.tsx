dd0689a01816c31c4ba03dda32aba76aed2e9172
anisotropy <j.hodoug@aroundus.com>
2022-05-10 15:52:56 +0900

    Set absolute path

---
+++ components/HomePage.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex items-center">
+      <div className="inline-flex p-6 mx-auto shadow-lg rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg bg-amber-500 text-white">
+        TailwindCSS
+      </div>
+    </div>
+  );
+};
+
+export default Home;
