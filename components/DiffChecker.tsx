0e5cb2de7779c5dba35147a9023b1e1742fb69e8
anisotropy <j.hodoug@aroundus.com>
2022-05-14 12:23:17 +0900

    Event hander for edit

--- components/DiffChecker.tsx
+++ components/DiffChecker.tsx
@@ -3,23 +3,33 @@ import makeDiff, {
   removeSentence,
   Char,
   diffWithoutSplit,
-  Sign,
   SingleLine,
 } from "utils/makeDiff";
 import classnames from "classnames";
 
-type DiffCharProps = { chars: Char[] };
+type DiffCharProps = {
+  chars: Char[];
+  cursorPointer: boolean;
+  onClick: () => void;
+};
 
-const DiffChars = ({ chars }: DiffCharProps) => {
+const DiffChars = ({ chars, cursorPointer, onClick }: DiffCharProps) => {
   return (
-    <div role="cell" className="grow whitespace-pre-wrap">
+    <div
+      role="cell"
+      className={classnames("grow", "whitespace-pre-wrap", {
+        "cursor-pointer": cursorPointer,
+      })}
+      onClick={onClick}
+    >
       {chars.map((char, i) => (
         <span
           key={i}
           role="cell"
-          className={
-            char.removed ? "bg-red-200" : char.added ? "bg-green-200" : ""
-          }
+          className={classnames({
+            "bg-red-200": char.removed,
+            "bg-green-200": char.added,
+          })}
         >
           {char.value}
         </span>
@@ -29,12 +39,18 @@ const DiffChars = ({ chars }: DiffCharProps) => {
 };
 
 type LineNumberProps = {
-  sign: Sign;
+  color: "red" | "green" | "gray";
+  cursorPointer: boolean;
   number: number | null;
   onClick: () => void;
 };
 
-const LineNumber = ({ sign, number, onClick }: LineNumberProps) => {
+const LineNumber = ({
+  color,
+  cursorPointer,
+  number,
+  onClick,
+}: LineNumberProps) => {
   return (
     <div
       role="cell"
@@ -45,10 +61,10 @@ const LineNumber = ({ sign, number, onClick }: LineNumberProps) => {
         "text-slate-500",
         "shrink-0",
         {
-          "bg-red-100 hover:bg-red-200": sign === "-",
-          "bg-green-100 hover:bg-green-200": sign === "+",
-          "bg-gray-100 hover:bg-gray-200": sign === null,
-          "cursor-pointer": sign !== null,
+          "bg-red-100 hover:bg-red-200": color === "red",
+          "bg-green-100 hover:bg-green-200": color === "green",
+          "bg-gray-100 hover:bg-gray-200": color === "gray",
+          "cursor-pointer": cursorPointer,
         }
       )}
       onClick={onClick}
@@ -62,12 +78,27 @@ type DiffRowProps = {
   line: SingleLine;
   onAdd: (line: SingleLine) => void;
   onRemove: (line: SingleLine) => void;
+  onEdit: (line: SingleLine) => void;
 };
 
-const DiffRow = ({ line, onAdd, onRemove }: DiffRowProps) => {
+const DiffRow = ({ line, onAdd, onRemove, onEdit }: DiffRowProps) => {
   const onClickNumber = () => {
     if (line.sign === "-") onAdd(line);
     if (line.sign === "+") onRemove(line);
+  };
+
+  const onClickSentence = () => {
+    if (line.sign !== "-") onEdit(line);
+  };
+
+  const numberProps = {
+    color:
+      line.sign === "-"
+        ? "red"
+        : line.sign === "+"
+        ? "green"
+        : ("gray" as "red" | "green" | "gray"),
+    cursorPointer: line.sign !== null,
   };
 
   return (
@@ -75,24 +106,29 @@ const DiffRow = ({ line, onAdd, onRemove }: DiffRowProps) => {
       role="row"
       className={classnames({
         flex: true,
-        "bg-red-50": line.sign === "-",
-        "bg-green-50": line.sign === "+",
+        "bg-red-50 hover:bg-red-100": line.sign === "-",
+        "bg-green-50 hover:bg-green-100": line.sign === "+",
+        "bg-white hover:bg-gray-100": line.sign === null,
       })}
     >
       <LineNumber
-        sign={line.sign}
+        {...numberProps}
         number={line.showLeftNumber ? line.leftNumber : null}
         onClick={onClickNumber}
       />
       <LineNumber
-        sign={line.sign}
+        {...numberProps}
         number={line.showRightNumber ? line.rightNumber : null}
         onClick={onClickNumber}
       />
       <div role="cell" className="w-8 text-center shrink-0">
         {line.sign}
       </div>
-      <DiffChars chars={line.chars} />
+      <DiffChars
+        chars={line.chars}
+        cursorPointer={line.sign !== "-"}
+        onClick={onClickSentence}
+      />
     </div>
   );
 };
@@ -119,10 +155,20 @@ const DiffChecker = ({ oldDoc, newDoc, onChangeArticle }: DiffCheckerProps) => {
     onChangeArticle(newSentece);
   };
 
+  const onEdit = () => {
+    console.log("onEdit");
+  };
+
   return (
     <div role="table" className="text-sm leading-relaxed font-mono">
       {lines.map((line, i) => (
-        <DiffRow key={i} line={line} onAdd={onAdd} onRemove={onRemove} />
+        <DiffRow
+          key={i}
+          line={line}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onEdit={onEdit}
+        />
       ))}
     </div>
   );
