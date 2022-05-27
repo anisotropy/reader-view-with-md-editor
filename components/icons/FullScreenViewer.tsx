3b7a86e3325e81af2aacfe0048ae32c46aef306e
anisotropy <j.hodoug@aroundus.com>
2022-05-27 19:20:45 +0900

    FullScreenViewer style

--- components/icons/FullScreenViewer.tsx
+++ components/icons/FullScreenViewer.tsx
@@ -6,17 +6,16 @@ type FullScreenViewerProps = { markdown: string; onShrink: () => void };
 
 const FullScreenViewer = (props: FullScreenViewerProps) => {
   return (
-    <div className="">
-      <div>
-        <Button
-          text="Shrink"
-          icon={<FullScreenMin />}
-          onClick={props.onShrink}
-        />
-      </div>
+    <div className="pb-12">
       <div className="p-4">
         <MarkdownViewer markdown={props.markdown} className="mx-auto" />
       </div>
+      <Button
+        text="Shrink"
+        icon={<FullScreenMin />}
+        onClick={props.onShrink}
+        className="fixed bottom-4 right-4 shadow-md"
+      />
     </div>
   );
 };
