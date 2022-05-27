d60bf3104ce8026e5181ca525b1db83cbf3e9a0b
anisotropy <j.hodoug@aroundus.com>
2022-05-28 00:46:14 +0900

    introduction text

--- components/App.tsx
+++ components/App.tsx
@@ -2,15 +2,18 @@ import classNames from "classnames";
 import React, { useState } from "react";
 import Body from "./Body";
 import Button from "./Button";
-import FullScreenMin from "./icons/FullScreenMim";
 import FullScreenViewer from "./icons/FullScreenViewer";
 import InputArticle from "./InputArticle";
-import MarkdownViewer from "./MarkdownViewer";
+
+const introduction = {
+  origin: `# Reader View with Markdown Editor\n\nYou can strip clutters from a webpage and read it on "Reader View" mode by Mozilla's open-source Readability.\nIf necessary contents are removed, you can edit them using text diff viewer and markdown editor.`,
+  readable: `# Reader View with Markdown Editor\n\nYou can strip clutters from a webpage and read it on "Reader View" mode.\nIf necessary contents are removed, you can edit them using text diff viewer and markdown editor.\n\nClick any line in "Markdown Editor" and add, remove or edit the line.`,
+};
 
 const App = () => {
   const [article, setArticle] = useState({
-    origin: "kkk\nccc\naaa",
-    readable: "kkz\naaa\nbbb",
+    origin: introduction.origin,
+    readable: introduction.readable,
   });
 
   const [showInput, setShowInput] = useState(false);
